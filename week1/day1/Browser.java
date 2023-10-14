aed1e4fbc4de84ad3d2db474177e17a5b505973a
SadhanaBalamurugan <147945222+SadhanaBalamurugan@users.noreply.github.com>
2023-10-14 15:17:52 -0400

    Add files via upload

---
+++ week1/day1/Browser.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Browser {
+	public static void main(String[] args) {
+		float browserVersion = 10.2f;
+		String browserName = "Edge";
+		boolean pageLoaded = true;
+		int noOfBrowser = 2;
+		char browserLogo = 'I';
+		System.out.println("BrowserVersion:" + browserVersion);
+		System.out.println("browserName:" + browserName);
+		System.out.println("pageLoaded:" + pageLoaded);
+		System.out.println("noOfBrowser:" + noOfBrowser);
+		System.out.println("browserLogo:" + browserLogo);
+		}
+
+}
